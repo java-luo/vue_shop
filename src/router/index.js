bf78cbf361b7ab632bc544a60bb2f7b7eb69915c
L <1551528143@qq.com>
2020-06-14 19:10:00 +0800

    完成登陆模块

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
-
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    redirect:"/login"
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/login",
+    component: ()=>import("../components/Login"),
+    meta:{
+      "title":"登陆"
+    }
+  },
+  {
+    path: "/home",
+    component: ()=> import("../components/Home"),
+    meta:{
+      "title":"Home"
+    }
   }
+
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode:"history",
 })
+//路由导航守卫 用来实现权限管理
+router.beforeEach((to,from,next)=>{
+  document.title=to.meta.title
+  if (to.path === "/login") return next()
+  let token = window.sessionStorage.getItem("token")
+  if (token != null) return next()
+  next("/login")
+
+})
+
 
 export default router
